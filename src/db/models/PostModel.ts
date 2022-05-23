95972289502ff8ec0a6e14efa12ea159dedee304
Micah Fang <micah.fang@outlook.com>
2022-05-23 12:37:34 +0800

    modify: post.statics.findByConstributor

--- src/db/models/PostModel.ts
+++ src/db/models/PostModel.ts
@@ -1,12 +1,14 @@
-import { Schema, model, Document, Model } from 'mongoose'
+import { Schema, model, Document, Model, Types, ObjectId } from 'mongoose'
 
 const modelName = 'Post'
 
 export interface IPost {
   title: string
-  contributor: string
+  contributor: ObjectId
   content: string
-  likeCount: number
+  likeCount?: number
+  createdAt: Date
+  updatedAt: Date
 }
 
 export interface IPostDocument extends IPost, Document {
@@ -15,17 +17,36 @@ export interface IPostDocument extends IPost, Document {
 
 export interface IPostModel extends Model<IPostDocument> {
   findByTitle: (title: string) => Promise<IPostDocument[]>
+  findByContributor: (name: string) => Promise<IPostDocument[]>
 }
 
-const schema = new Schema({
-  title: String,
-  contributor: String,
+const schema = new Schema<IPostDocument, IPostModel>({
+  title: {
+    type: String,
+    required: true,
+  },
+  contributor: {
+    type: Types.ObjectId,
+    immutable: true,
+  },
   content: String,
   likeCount: Number,
+  createdAt: {
+    type: Date,
+    default: () => Date.now(),
+  },
+  updatedAt: {
+    type: Date,
+    default: () => Date.now(),
+  },
 })
 
 schema.statics.findByTitle = function (title: string) {
   return this.find({ title: new RegExp(title, 'i') })
+}
+
+schema.statics.findByContributor = function (name: string) {
+  return this.find({ contributor: new RegExp(name, 'i') })
 }
 
 schema.methods.print = function () {
